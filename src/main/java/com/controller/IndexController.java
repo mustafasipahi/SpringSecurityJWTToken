de8f7554dcabaeb911cb4ef32751317b8622ce1f
Mustafa Sipahi <mustafa.sipahi@demirorenteknoloji.com>
2023-03-13 21:32:43 +0300

    f

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class IndexController implements ErrorController {
+
+    @RequestMapping(value = "/error")
+    public RedirectView error() {
+        return new RedirectView("/home");
+    }
+}
